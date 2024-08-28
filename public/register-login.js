c4f9e0e4cf01c2d59ee0983176d9905d3cbcdc1e
Malachi <mpeel9938@gmail.com>
2024-08-28 16:12:53 -0700

    test

---
+++ public/register-login.js
@@ -0,0 +1,6 @@
+const SignInButton = document.getElementById("SignInButton");
+const SignUpButton = document.getElementById("SignUpButton");
+const SignInForm = document.getElementById("SignIn");
+const SignUpForm = document.getElementById("SignUp");
+
+addEventListener
