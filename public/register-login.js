85960e5fe9477407dedf27de41cbab658817f085
Malachi <mpeel9938@gmail.com>
2024-08-29 14:04:32 -0700

    readMe updates // register/login form buttons working

--- public/register-login.js
+++ public/register-login.js
@@ -1,6 +1,14 @@
 const SignInButton = document.getElementById("SignInButton");
 const SignUpButton = document.getElementById("SignUpButton");
-const SignInForm = document.getElementById("SignIn");
-const SignUpForm = document.getElementById("SignUp");
+const SignInForm = document.getElementById("login-form");
+const SignUpForm = document.getElementById("register-form");
 
-addEventListener
+SignUpButton.addEventListener('click',function(){
+    SignInForm.style.display='none';
+    SignUpForm.style.display='block';
+})
+
+SignInButton.addEventListener('click',function(){
+    SignInForm.style.display='block';
+    SignUpForm.style.display='none'; 
+})
