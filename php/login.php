3186e90c48ccce5bed793345a6641590e38c89b0
Malachi <mpeel9938@gmail.com>
2024-09-19 14:20:44 -0700

    overhaul // project converted to a movie sharing platform instead of album sharing. Foundation is still the same, just need to rework the database // Reason for change was becasue i'm working on a similar project with react

--- php/login.php
+++ php/login.php
@@ -1,6 +1,17 @@
 <?php
 
+
+
+include 'connect.php';
+
 # Start Log in Session
 session_start(); 
 
+if (isset($_POST['LogIn'])){
+    $email=$_POST['email'];
+    $password=$_POST['password']
+
+    $sql = "SELECT * FROM users WHERE email='$email' and password='$password'";
+
+}
 ?>
