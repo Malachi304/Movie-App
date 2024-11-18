b7dcd333712edcdf10e2aa9960b06a0c78b9fe33
Malachi <mpeel9938@gmail.com>
2024-11-18 13:05:06 -0800

    removed unused files

--- backend/php/addMovie.php
+++ backend/php/addMovie.php
@@ -10,6 +10,37 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $rating = $_POST['rating'];
 }
 
+# We want to instert the data into both the database and the JSON file(backup)
+
+# Inster into JSON
+$JSONfile = file_get_contents('../data.JSON');
+
+# Decode the JSON data into an php array
+$movies = json_decode($JSONfile, true);
+
+if($movies === null){
+    die("JSON data could not be decoded");
+}
+else{
+    # Create movie object
+    $newMovie = [
+        'title' => $title,
+        'director' => $director,
+        'year' => $year,
+        'genre' => $genre,
+        'rating' => $rating
+    ];
+
+    # Check if the movie already exists (Yes this is re)
+
+
+}
+
+
+# This will be used to insert the data into the database.
+# The ? will be replaced with the values from the array 
+$statement = $conn->prepare("INSERT INTO moviecollection (title, director, year, genre, rating) VALUES (?, ?, ?, ?, ?)");
+
 
 $conn->close();
 ?>
--- movie_app.session.sql
+++ movie_app.session.sql
@@ -1,1 +1,11 @@
-SELECT * FROM users
+SELECT * FROM moviecollection
+
+DELETE FROM moviecollection
+ALTER TABLE moviecollection ADD CONSTRAINT unique_movie UNIQUE (title, year);
+DESCRIBE moviecollection
+
+EXPLAIN moviecollection
+
+SHOW CREATE TABLE moviecollection
+
+TRUNCATE TABLE moviecollection
